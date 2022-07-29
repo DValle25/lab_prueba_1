19ae65d362eff38563819ce5f6069459b12b3979
David Vallejo <davallejo@uninorte.edu.co>
2022-07-28 20:10:35 -0500

    Esta version incluye una funcion

--- lab_prueba_1.py
+++ lab_prueba_1.py
@@ -4,3 +4,6 @@ print("y este mensaje es para decirte que dominare el mundo")
 # esto es un comentario
 # control K+C (convertir una linea en un comentario)
 # siempre guardar para que se pueda ejecutar lo que escribiste 
+def mi_primera_funcion():
+    print("Esta es mi primera funcion")
+mi_primera_funcion()
